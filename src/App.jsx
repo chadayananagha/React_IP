43dfdb02a8b52aa6031ecce727e95a7f148e9ba1
Anagha Chadayan <anagha.chadayan@gmail.com>
2024-04-07 12:04:46 +0200

    map added

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,88 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import { MapContainer, TileLayer } from 'react-leaflet';
+import { DateTime } from 'luxon';
+import Loading from './components/Loading';
+import 'leaflet/dist/leaflet.css';
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [ipInfo, setIpInfo] = useState(null);
+  const [countryInfo, setCountryInfo] = useState(null);
+  const [timeNow, setTimeNow] = useState(DateTime.local().c);
 
-  return (
+  useEffect(() => {
+    const getApiData = async () => {
+      try {
+        const ipData = await axios(
+          `https://geo.ipify.org/api/v2/country,city?apiKey=${
+            import.meta.env.VITE_GEO_API_KEY
+          }`
+        );
+
+        const countryData = await axios(
+          `https://restcountries.com/v3.1/alpha/de`
+        );
+
+        setIpInfo(ipData.data);
+        setCountryInfo(countryData.data[0]);
+      } 
+      catch (error) {
+        console.error(error);
+      }
+    };
+
+    getApiData();
+
+    const intervalId = setInterval(() => setTimeNow(DateTime.local().c), 1000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
+  return ipInfo ? (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <MapContainer
+        center={[ipInfo.location.lat, ipInfo.location.lng]}
+        zoom={13}
+        scrollWheelZoom={false}
+        className='h-screen'
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+        />
+      </MapContainer>
+      <div className='fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-[99999999] card bg-base-100 shadow-xl rounded-xl overflow-hidden w-96'>
+        <img src={countryInfo.flags.png} alt={countryInfo.flags.alt} />
+        <div className='card-body text-center'>
+          <h2 className='card-title block'>Your IP is: {ipInfo.ip}</h2>
+          <p>
+            You are currently located in {ipInfo.location.city},{' '}
+            {countryInfo.name.common}
+          </p>
+          <hr className='my-2' />
+          <p className='font-light'>
+            <span role='img' aria-label='calendar emoji'>
+              📅
+            </span>
+            Today is the{' '}
+            {new Date(Date.now()).getDate().toString().padStart(2, '0')}.
+            {(new Date(Date.now()).getMonth() + 1).toString().padStart(2, '0')}.
+            {new Date(Date.now()).getFullYear()}
+          </p>
+          <p className='font-light'>
+            <span role='img' aria-label='clock emoji'>
+              🕑
+            </span>
+            Your local time is {timeNow.hour.toString().padStart(2, '0')}:
+            {timeNow.minute.toString().padStart(2, '0')}:
+            {timeNow.second.toString().padStart(2, '0')}
+          </p>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
-}
+  ) : (
+    <Loading />
+  );
+};
 
-export default App
+export default App;
