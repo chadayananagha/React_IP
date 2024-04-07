498c9eb69f504f17e935d5d0408f817f1537dae3
Anagha Chadayan <anagha.chadayan@gmail.com>
2024-04-07 21:15:48 +0200

    loading component added

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,8 +1,8 @@
 const Loading = () => {
     return (
       <div className='h-screen flex justify-center items-center'>
-        <p className='text-3xl'>Loading your IP</p>
-        <span className='loading loading-dots loading-sm mt-6'></span>
+        <p className='text-xl'>Loading....</p>
+        {/* <span className='loading loading-dots loading-sm mt-6'></span> */}
       </div>
     );
   };
